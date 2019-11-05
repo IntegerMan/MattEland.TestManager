43eb2e037be4792b233ee7a6a7654c095adc7ea1
Matt Eland <Matt.Eland@GMail.com>
2019-11-04 22:57:51 -0500

    Add project files.

---
+++ MattEland.TestManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MattEland.TestManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
